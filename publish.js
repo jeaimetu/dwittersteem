0db3ecb7b4cf4696540fb810a365dc4edd9650f2
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-23 22:03:13 +0900

    Update publish.js

--- publish.js
+++ publish.js
@@ -4,19 +4,25 @@ const url = process.env.MONGODB_URI;
 
 
 function publishData(){
+	var msg = "Dabble의 짧은 글을 모아 보았습니다.";
   	MongoClient.connect(url, function(err, db) {
 		var dbo = db.db("heroku_dg3d93pq");
 		var tod = Date.now() - 1000*60*60*8;
 		const findquery = { date : {$gt:tod} };
 		dbo.collection("board").find(findquery).toArray(function(err, result){
 			for(i = 0;i<result.length;i++){
-				console.log(result[i].data);
-			}			
+				msg += "<h3>";
+				msg+= result[i].account;
+				msg += "</h3>" + "<br>;				
+				msg += result[i].data;
+				msg += "<br><br>" + "***"";
+			}	
+			return msg;
 		});
 	});
 }
 
-publishData();
+console.log(publishData());
 		
 		
 		
