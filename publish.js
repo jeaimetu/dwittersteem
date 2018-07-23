6df345829c90f3cad183d6478f5ad81883a5a852
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-23 21:53:18 +0900

    Update publish.js

--- publish.js
+++ publish.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI;
 function publishData(){
   	MongoClient.connect(url, function(err, db) {
 		var dbo = db.db("heroku_dg3d93pq");
-		var tod = Date.now() - 1000*60*60*4;
+		var tod = Date.now() - 1000*60*60*8;
 		const findquery = { date : {$gt:tod} };
 		dbo.collection("board").find(findquery).toArray(function(err, result){
 			for(i = 0;i<result.length;i++){
