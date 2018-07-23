e66f782d5bbccb298278f67fef1260a8513ede14
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-24 08:13:32 +0900

    Update publish.js

--- publish.js
+++ publish.js
@@ -12,14 +12,15 @@ function writingReply(data, id){
 	var parent_permlink = "kr-dev";
 	var json_metadata = '';
 	//check author have . then remove that
-	var parent_author_permlink = "jeaimetu";
+	var parent_author_permlink = "dabble";
 	
 	if(data == null){
 		data = "you missed something!!!!";
 	}
 
 
-	const permlink = steem.formatter.commentPermlink(parent_author_permlink, parent_permlink)
+	//const permlink = steem.formatter.commentPermlink(parent_author_permlink, parent_permlink)
+	const perlink = Date.now() + "dabble";
 	//const permlink = steem.formatter.commentPermlink('jeaimetu', parent_permlink)
 	var content = data;
 	
@@ -29,7 +30,7 @@ function writingReply(data, id){
     	parent_permlink,      // main tag for new blog post
     	'dabble',               // same user the private_posting_key is for
     	permlink,             // a slug (lowercase 'a'-'z', '0'-'9', and '-', min 1 character, max 255 characters)
-    	'Dabble 퍼블리슁',                // human-readable title
+    	'Dabble 퍼블리슁 2',                // human-readable title
     	content,                 // body of the post or comment
     	json_metadata,          // arbitrary metadata
 		function (err, result){
@@ -47,18 +48,20 @@ function writingReply(data, id){
 
 
 function publishData(){
-	var msg = "Dabble의 짧은 글을 모아 보았습니다.<br><br>";
+	var msg = "![](https://cdn.steemitimages.com/DQmTuuRkSahdiMaH1X4esGJNUFaqdcA8v2qWn1ZJn7DGKsr/image.png)";
+	msg += "Dabble의 짧은 글을 모아 보았습니다.<br><br>";
   	MongoClient.connect(url, function(err, db) {
 		var dbo = db.db("heroku_dg3d93pq");
-		var tod = Date.now() - 1000*60*60*8;
-		const findquery = { date : {$gt:tod} };
+		var tod = Date.now() - 1000*60*60*24;
+		var tod1 = Date.now() - 1000*60*60*9;
+		const findquery = { date : {$gt:tod, $lt:tod1} };
 		dbo.collection("board").find(findquery).toArray(function(err, result){
 			for(i = 0;i<result.length;i++){
 				msg += "<h3>";
 				msg+= result[i].account;
 				msg += "</h3>" + "<br>";				
 				msg += result[i].data;
-				msg += "<br><br>" + "***";
+				msg += "<br><br>" + "\n" + "***" + "\n";
 			}	
 			console.log(msg);
 			if(process.env.pub == true){
