3d50af3b4d940300028184a9211f44479d29a9c1
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-23 21:43:29 +0900

    Update publish.js

--- publish.js
+++ publish.js
@@ -1,1 +1,23 @@
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGODB_URI;
 
+
+function publishData(){
+  	MongoClient.connect(url, function(err, db) {
+		var dbo = db.db("heroku_dg3d93pq");
+		var tod = Date.now();
+		
+		dbo.collection("board").find(findquery).toArray(function(err, result){
+			for(i = 0;i<result.length;i++){
+				console.log(result[i].data);
+			}			
+		});
+	});
+}
+
+publishData();
+		
+		
+		
+	  
