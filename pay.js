d9e5c26675379cac581741b987c79e10a1c8199f
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-07-25 19:23:55 +0900

    Update pay.js

--- pay.js
+++ pay.js
@@ -19,7 +19,7 @@ function getUserVoting(){
 	var sumVoting = (db, callback) => {
 		var agr = [
 			{$match: {account: {$exists:true, $ne: null}}},
-			{$group: {_id:"$account", all : { $sum : $voting}}}];
+			{$group: {_id:"$account", all : { $sum : "$voting"}}}];
 		var dbo = db.db("heroku_dg3d93pq");
 		var cursor = dbo.collection('board').aggregate(agr).toArray( (err, res) => {
 			console.log(res);
