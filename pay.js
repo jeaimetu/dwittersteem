3cfe8aa63064cd1e5535eca88655ef06c13cfd6a
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-07-26 11:38:56 +0900

    Update pay.js

--- pay.js
+++ pay.js
@@ -7,25 +7,43 @@ var url = process.env.MONGODB_URI;
 
 
 
-
+var totalUser = 0;
+var totalSumOfVoting = 0;
+const votingFactor = 3;
 
 function getUserVoting(){
 	MongoClient.connect(url, (err, db) => {
 		sumVoting(db, (result) => {
 			db.close();
-			console.log("getRank",result);
+			console.log("getUserVoting", result);
 		});
 	});
 	var sumVoting = (db, callback) => {
 		var agr = [
 			{$match: {account: {$exists:true, $ne: null}}},
-			{$group: {_id:"$account", all : { $sum : "$voting"}}}];
+			{$group: {_id:"$account", vote : { $sum : "$voting"}}}];
 		var dbo = db.db("heroku_dg3d93pq");
 		var cursor = dbo.collection('board').aggregate(agr).toArray( (err, res) => {
 			console.log(res);
+			totalUser = res.length;
+			totalSumOfVoting = 0;
+			for(i = 0;i < res.length; i++)
+				totalSumOfVoting += res[i].vote;
+			//update each users token in their wallet
+			for(i = 0; i > res.length;i++){
+				const updatequery = {eosid : res[i].account};
+				tokenSize = res[i].vote / totalSumOfVoting + 1;
+				const myobj = { $set : {wallet : tokenSize}};
+				dbo.collection('user').updateOne(updatequery, myobj, (err,res) =>{					
+					if(err) throw err;
+					console.log("update wallet", res[i].account, tokenSize);
+				});
+			}
 		});
 	};
 }
+
+
 
 
 	
