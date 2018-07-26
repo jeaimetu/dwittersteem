59b75b3ef51572776d0e7331336cb3b7ff05caee
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-07-26 12:08:14 +0900

    Update pay.js

--- pay.js
+++ pay.js
@@ -47,12 +47,18 @@ function setWallet(account, vote){
 		const dbo = db.db("heroku_dg3d93pq");
 		const findQuery = {account : account};
 		dbo.collection('user').findOne(findQuery, (err, result) => {
-			if(err) throw err;
+			if(err){ 
+				throw err;
+				console.log(err);
+			}
 			const updatequery = {account : account};
 			tokenSize = (vote / totalSumOfVoting + 1) + result.wallet;
 			const myobj = { $set : {wallet : tokenSize}};
 			dbo.collection('user').updateOne(updatequery, myobj, (err,res) =>{
-				if(err) throw err;
+				if(err){ 
+					throw err;
+					console.log(err);
+				}
 				console.log("update wallet", acc, tokenSize);
 				db.close();
 			});
