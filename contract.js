f7db60269da001f69fe000c4ef5ef7b52d9317dd
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-07-27 17:25:57 +0900

    Update contract.js
    initial contract for DAB.

--- contract.js
+++ contract.js
@@ -23,6 +23,28 @@ abi = fs.readFileSync("abiUrl");
 
 eos.setcode('myaccount', 0, 0, wasm) // @returns {Promise}
 eos.setabi('myaccount', JSON.parse(abi)) // @returns {Promise}
+
+function createToken(account){
+  await eos.transaction(account, myaccount => {
+
+  // Create the initial token with its max supply
+  // const options = {authorization: 'myaccount'} // default
+    //500,000,000 is the base. Annual inflation is 5% to 5 years. Others will be burned.
+  myaccount.create(account, '1000000000.0000 DAB')//, options)
+
+  // Issue some of the max supply for circulation into an arbitrary account
+  myaccount.issue(account, '1000000.0000 DAB', 'issue')
+})
+
+  const balance = await eos.getCurrencyBalance(account, account, 'DAB')
+  console.log('Currency Balance', balance)
+}
+
+function transfer(from, to, amount){
+	const myaccount = await eos.contract(from);
+	await myaccount.transfer(from, to, amount + " " + "DAB","");
+}
+
                                  
                                                                                                                      
                                                                                                                      
