1d8b00b7855a5d95d36d4bd88d99dff1298bfefb
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-10 13:04:00 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -55,7 +55,7 @@ async function createToken(account){
   // Create the initial token with its max supply
   // const options = {authorization: 'myaccount'} // default
     //500,000,000 is the base. Annual inflation is 5% to 5 years. Others will be burned.
-  myaccount.create(account, ' 11000000000.0000 BEANS')//, options)
+  myaccount.create(account, '11000000000.0000 BEANS')//, options)
 
   // Issue some of the max supply for circulation into an arbitrary account
   myaccount.issue(account, '10000000000.0000 BEANS', 'issue')
