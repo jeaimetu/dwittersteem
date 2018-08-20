05cd94daddff9b1c6b12c488c0c2340c204a4a4a
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-20 18:09:37 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,7 +1,7 @@
-const wasmUrl = "./contract/eosio.token.wasm";
-const abiUrl = "./contract/eosio.token.abi";
+const wasmUrl = "./newcontract/eosio.token.wasm";
+const abiUrl = "./newcontract/eosio.token.abi";
 
-const account = "awesometeddy";
+const account = "eoscafekorea";
 
 Eos = require('eosjs');
 const fs = require('fs');
@@ -17,10 +17,10 @@ var url = process.env.MONGODB_URI;
 
 config = {
   //chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
-  chainId: "00000001536fe4bec69c38d68e1b3048ac6896d12a9007ad0d471a575380d82a",// 32 byte (64 char) hex string
+  chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",// 32 byte (64 char) hex string
   keyProvider: process.env.key, // WIF string or array of keys..
   //httpEndpoint: 'https://mainnet.eoscalgary.io',
-  httpEndpoint:	"http://jungle.cryptolions.io:18888",
+  httpEndpoint:	"http://193.93.219.219:8888",
   expireInSeconds: 60,
   broadcast: true,
   verbose: false, // API activity
@@ -95,8 +95,8 @@ abi = fs.readFileSync(abiUrl);
 
 //console.log("Wasm", wasm);
 //console.log("Abi", abi);
-eos.setcode("awesometeddy", 0, 0, wasm) // @returns {Promise}
-eos.setabi("awesometeddy", JSON.parse(abi)) // @returns {Promise}
+eos.setcode("eoscafekorea", 0, 0, wasm) // @returns {Promise}
+eos.setabi("eoscafekorea", JSON.parse(abi)) // @returns {Promise}
 
 
 
@@ -126,10 +126,10 @@ async function transfer(from, to, amount){
 */
 
 
-createToken("awesometeddy");
+createToken("eoscafekorea");
 
-eos.getCurrencyBalance("awesometeddy", "awesometeddy", 'DAB').then(function(result){
-	console.log("awesometeddy balance", result);
+eos.getCurrencyBalance("eoscafekorea", "eoscafekorea", 'DAB').then(function(result){
+	console.log("eoscafekorea balance", result);
 });
                
 
