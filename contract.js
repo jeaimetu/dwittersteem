d708efdfebc900a63ca94e6d5326c1a984800156
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-21 09:21:00 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,5 +1,5 @@
-const wasmUrl = "./newcontract/eosio.token.wasm";
-const abiUrl = "./newcontract/eosio.token.abi";
+const wasmUrl = "./leapcontract/eosio.token.wasm";
+const abiUrl = "./leapcontract/eosio.token.abi";
 
 const account = "eoscafekorea";
 
@@ -95,8 +95,8 @@ abi = fs.readFileSync(abiUrl);
 
 //console.log("Wasm", wasm);
 //console.log("Abi", abi);
-//eos.setcode("eoscafekorea", 0, 0, wasm) // @returns {Promise}
-//eos.setabi("eoscafekorea", JSON.parse(abi)) // @returns {Promise}
+eos.setcode("eoscafekorea", 0, 0, wasm) // @returns {Promise}
+eos.setabi("eoscafekorea", JSON.parse(abi)) // @returns {Promise}
 
 
 
@@ -108,10 +108,10 @@ async function createToken(account){
   // Create the initial token with its max supply
   // const options = {authorization: 'myaccount'} // default
     //500,000,000 is the base. Annual inflation is 5% to 5 years. Others will be burned.
-  myaccount.create(account, '40000000000.0000 PUB')//, options)
+  myaccount.create(account, '40000000000.0000 BEANS')//, options)
 
   // Issue some of the max supply for circulation into an arbitrary account
-  myaccount.issue(account, '10000000000.0000 PUB', 'issue')
+  //myaccount.issue(account, '10000000000.0000 PUB', 'issue')
 })
 
   //const balance = await eos.getCurrencyBalance(account, account, 'DAB')
@@ -128,7 +128,7 @@ async function transfer(from, to, amount){
 
 createToken("eoscafekorea");
 
-eos.getCurrencyBalance("eoscafekorea", "eoscafekorea", 'BEAN').then(function(result){
+eos.getCurrencyBalance("eoscafekorea", "eoscafekorea", 'BEANS').then(function(result){
 	console.log("eoscafekorea balance", result);
 });
                
