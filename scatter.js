4dd47f3da0a842bc3ba1ed68dc9f06ce0bec94fa
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-26 06:33:35 +0900

    Update scatter.js

--- scatter.js
+++ scatter.js
@@ -1,1 +1,19 @@
+const scatter = require("eos-scatter");
 
+const network = {
+    blockchain:'eos',
+    host:'mainnet.eoscalgary.io',
+    port:80,
+    protocol:'https',
+    chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+};
+
+const eosOptions = {};
+
+
+const eos = scatter.eos( network, Eos, eosOptions, 'https' );
+
+
+eos.contract('hello').then(contract => {
+    contract.hi(...args)
+});
