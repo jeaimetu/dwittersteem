841de7a231fca8a80f529ac4ecf5a8f079549110
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-28 00:21:15 +0900

    Update pay.js

--- pay.js
+++ pay.js
@@ -10,8 +10,8 @@ var url = process.env.MONGODB_URI;
 var totalUser = 0;
 var totalSumOfVoting = 0;
 const votingFactor = 3;
-const distributionForDay = 7000;
-const postingDistributionForDay = 7000;
+const distributionForDay = 10000;
+const postingDistributionForDay = 10000;
 
 function getUserVoting(){
 	MongoClient.connect(url, (err, db) => {
@@ -156,7 +156,7 @@ function checkTime(){
 function airdropByWriting(){
 	MongoClient.connect(url, (err, db) => {
 		const dbo = db.db("heroku_dg3d93pq");
-		var tod = Date.now() - 1000*60*60*33;
+		var tod = Date.now() - 1000*60*60*16;
 		var tod1 = Date.now();
 
 		const agr = [
