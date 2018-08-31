d3a465fbc2793baac2104083278bada3c281ca97
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-31 17:23:20 +0900

    Update main.js

--- scatter2/main.js
+++ scatter2/main.js
@@ -140,7 +140,23 @@ function refreshKeys() {
         });
 }
 
-function vote () {
+function vote(){
+    const eos = getEos();
+    var account = document.getElementById('eos-account').value;
+    //eosScatter = scatter.eos(network, Eos, eosOptions, "http");
+    const account = scatter.identity.accounts.find(x => x.blockchain === "eos")
+    const options = {authorization:[{actor:account, permission:account.authority }] };
+    
+    eosScatter.contract('elements').then(contract => {
+        contract.transfer(account,"awesometeddy", "2000.0000 DAB", "Scatter test");
+    })
+    .catch(err => {
+        console.log("error");
+    });
+}
+
+
+function vote2 () {
     var eos = getEos();
 
     var selectedBPs = getSelectedBPs();
