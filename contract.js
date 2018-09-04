0b62250fc0ca6cae4bff17b4a2ad176823b6e5b2
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-04 21:51:29 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -97,8 +97,8 @@ abi = fs.readFileSync(abiUrl);
 
 //console.log("Wasm", wasm);
 //console.log("Abi", abi);
-eos.setcode("thebeantoken", 0, 0, wasm) // @returns {Promise}
-eos.setabi("thebeantoken", JSON.parse(abi)) // @returns {Promise}
+eos.setcode("honeydacserv", 0, 0, wasm) // @returns {Promise}
+eos.setabi("honeydacserv", JSON.parse(abi)) // @returns {Promise}
 
 
 
