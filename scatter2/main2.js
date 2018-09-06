a62e85a64d49d4d1b563c6e17421b39e53022ffb
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-07 02:15:56 +0900

    Update main2.js

--- scatter2/main2.js
+++ scatter2/main2.js
@@ -23,9 +23,9 @@ function toggleKeyInput () {
       const eosOptions = { expireInSeconds:60 };
       const eos = scatter.eos(network, Eos, eosOptions);
       
-      const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
+      //const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
       
-      eos.transfer(account.name, 'eoscafekorea', '1.0000 DAB', 'memo', transactionOptions).then(trx => {
+      eos.transfer(account.name, 'eoscafekorea', '1.0000 DAB', '').then(trx => {
         console.log(`Transaction ID: ${trx.transaction_id}`);
       }).catch(error => {
         console.error(error);
