ffa510e57cc1626ad9e127b95f5c8fc7cb3f0a72
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-06 15:52:32 +0900

    Update main2.js

--- scatter2/main2.js
+++ scatter2/main2.js
@@ -3,7 +3,7 @@
 const network = {
     blockchain:'eos',
     protocol:'https',
-    host:'nodes.get-scatter.com',
+    host:'mainnet.eoscalgary.io',
     port:443,
     chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 }
