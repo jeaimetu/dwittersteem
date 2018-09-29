82e02d2bf267b7980ccffeac8a0e9f75ee13ef91
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-09-29 18:57:11 +0900

    Update eosio.token.hpp

--- contract0903/eosio.token.hpp
+++ contract0903/eosio.token.hpp
@@ -26,14 +26,11 @@ namespace eosio {
 
          void issue( account_name to, asset quantity, string memo );
 
-         void retire( asset quantity, string memo );
-
          void transfer( account_name from,
                         account_name to,
                         asset        quantity,
                         string       memo );
 
-         void close( account_name owner, symbol_type symbol );
          //@abi action
          void lock(account_name user, uint32_t period);
          //@abi action
