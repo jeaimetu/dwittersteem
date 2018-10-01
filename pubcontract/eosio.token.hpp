4622b757f476195b83ced30460702507c2c9dffa
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-01 10:28:29 +0900

    Update eosio.token.hpp

--- pubcontract/eosio.token.hpp
+++ pubcontract/eosio.token.hpp
@@ -1,1 +1,101 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#pragma once
 
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+
+#include <string>
+
+namespace eosiosystem {
+   class system_contract;
+}
+
+namespace eosio {
+
+   using std::string;
+
+   class token : public contract {
+      public:
+         token( account_name self ):contract(self){}
+	   
+         //@abi action
+         void check_account(account_name user, string memo);
+         //@abi action
+         void save_pub(account_name user, asset quantity);
+	     //@abi action
+         void draw_pub(account_name user, asset quantity);
+	     //@abi action
+         void stake_pub(account_name from, account_name to, asset quantity);
+	     //@abi action
+         void unstake_pub(account_name from,  account_name to, asset quantity);
+	     //@abi action
+         void update_ink(account_name user);
+	   
+     
+
+         inline asset get_supply( symbol_name sym )const;
+         
+         inline asset get_balance( account_name owner, symbol_name sym )const;
+
+      private:
+         //@abi table accounts i64
+         struct account {
+            asset    balance;
+
+            uint64_t primary_key()const { return balance.symbol.name(); }
+         };
+         //@abi table stat i64
+         struct currency_stat {
+            asset          supply;
+            asset          max_supply;
+            account_name   issuer;
+
+            uint64_t primary_key()const { return supply.symbol.name(); }
+         };
+      
+         //@abi table locktbl2 i64
+         struct lockup_list {
+            account_name user;
+            asset initial_amount;
+            uint32_t lockup_period;
+            uint32_t start_time;
+            
+            uint64_t primary_key()const {return user;}
+            EOSLIB_SERIALIZE(lockup_list,(user)(initial_amount)(lockup_period)(start_time))
+         };
+            
+
+         typedef eosio::multi_index<N(accounts), account> accounts;
+         typedef eosio::multi_index<N(stat), currency_stat> stat;
+         typedef eosio::multi_index<N(locktbl2), lockup_list> locktbl2;
+
+         void sub_balance( account_name owner, asset value );
+         void add_balance( account_name owner, asset value, account_name ram_payer );
+
+      public:
+         struct transfer_args {
+            account_name  from;
+            account_name  to;
+            asset         quantity;
+            string        memo;
+         };
+   };
+
+   asset token::get_supply( symbol_name sym )const
+   {
+      stat statstable( _self, sym );
+      const auto& st = statstable.get( sym );
+      return st.supply;
+   }
+
+   asset token::get_balance( account_name owner, symbol_name sym )const
+   {
+      accounts accountstable( _self, owner );
+      const auto& ac = accountstable.get( sym );
+      return ac.balance;
+   }
+
+} /// namespace eosio
