b692cfbae60f9078a078ed37795c6d68aeec9e27
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-04 17:52:19 +0900

    Update eosio.token.cpp

--- pubcontract/eosio.token.cpp
+++ pubcontract/eosio.token.cpp
@@ -21,7 +21,7 @@ void token::transfer(account_name from, bool internalfrom, account_name to, bool
 	//internal to external
 	draw(from, balance);
 
-         INLINE_ACTION_SENDER(N(publytokenio), "transfer", {from, N(active)},{N(publytokenio), to, balance, std::string("refund bid on name ")});
+         INLINE_ACTION_SENDER(*this, transfer, {from, N(active)},{N(publytokenio), to, balance, std::string("refund bid on name ")});
 
 		 
 
