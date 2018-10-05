f2ee7a5392f6ebb62320a646bd9612d9536b93eb
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-05 17:41:33 +0900

    Update eosio.token.cpp

--- pubcontract/eosio.token.cpp
+++ pubcontract/eosio.token.cpp
@@ -31,10 +31,11 @@ void token::transfer(account_name from, bool internalfrom, account_name to, bool
 	save(to, balance);
         */
 	//external to external case
-	/*
+	
 	action(permission_level{ from, N(active) }, N(eoscafekorea), N(transfer),
 				 std::make_tuple(from, to, balance, "PUB transfer" )).send();
-	*/
+	
+	printf("for contract evaluation");
 
 						     
 }
