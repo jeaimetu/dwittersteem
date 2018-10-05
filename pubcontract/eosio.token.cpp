020f002b5c20fda6350a0d082b4220fe964a150a
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-05 15:57:10 +0900

    Update eosio.token.cpp

--- pubcontract/eosio.token.cpp
+++ pubcontract/eosio.token.cpp
@@ -117,31 +117,7 @@ void token::update(account_name from){
 
 	
 
-void token::sub_balance( account_name owner, asset value ) {
-   accounts from_acnts( _self, owner );
 
-   const auto& from = from_acnts.get( value.symbol.name(), "no balance object found" );
-   eosio_assert( from.balance.amount >= value.amount, "overdrawn balance" );
-
-   from_acnts.modify( from, owner, [&]( auto& a ) {
-         a.balance -= value;
-      });
-}
-
-void token::add_balance( account_name owner, asset value, account_name ram_payer )
-{
-   accounts to_acnts( _self, owner );
-   auto to = to_acnts.find( value.symbol.name() );
-   if( to == to_acnts.end() ) {
-      to_acnts.emplace( ram_payer, [&]( auto& a ){
-        a.balance = value;
-      });
-   } else {
-      to_acnts.modify( to, 0, [&]( auto& a ) {
-        a.balance += value;
-      });
-   }
-}
 
 }
 
