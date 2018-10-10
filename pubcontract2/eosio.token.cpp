57bba2d786fece42f185dcd40632282c86b61635
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-10 13:23:17 +0900

    Update eosio.token.cpp

--- pubcontract2/eosio.token.cpp
+++ pubcontract2/eosio.token.cpp
@@ -361,4 +361,4 @@ void token::add_balance( account_name owner, asset value, account_name ram_payer
 
 } /// namespace eosio
 
-EOSIO_ABI( eosio::token, (create)(issue)(transfer)(lock)(unlock)(newaccount)(check))
+EOSIO_ABI( eosio::token, (create)(issue)(transfer)(lock)(unlock)(newaccount)(check)(save)(draw)(pubtransfer)(itransfer)(stake)(unstake))
