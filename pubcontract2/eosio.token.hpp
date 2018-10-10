102eb0d46440975d27cfe684e907bd63506c396b
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-10-10 09:03:51 +0900

    Update eosio.token.hpp

--- pubcontract2/eosio.token.hpp
+++ pubcontract2/eosio.token.hpp
@@ -66,11 +66,51 @@ namespace eosio {
             uint64_t primary_key()const {return user;}
             EOSLIB_SERIALIZE(lockup_list,(user)(initial_amount)(lockup_period)(start_time))
          };
+      
+         //@abi table pubtbl i64
+         struct pub_table {
+            account_name user;
+            account_name eos_account
+            asset balance;
+	         asset ink;	         
+            
+            uint64_t primary_key()const {return user;}
+            EOSLIB_SERIALIZE(pub_table,(user)(balance)(ink))
+         };
+	      //@abi table staketbl i64
+	      struct stake_table {
+		      account_name user;
+            bool user_internal;
+		      account_name owner;
+            bool owner_internal;
+		      asset balance;
+		      uint32_t staked_at;
+		   
+		      uint64_t primary_key()const {return user;}
+		      EOSLIB_SERIALIZE(stake_table,(user_internal)(owner)(owner_internal)(balance)(staked_at))
+	      }; 
+	   
+	      //@abi table unstaketbl i64
+	      struct unstake_table {
+		      account_name user;
+            bool user_internal;
+            account_name from;
+            bool from_internal;
+		      asset balance;
+		      uint32_t unstaked_at;
+		   
+		      uint64_t primary_key()const {return user;}
+		      EOSLIB_SERIALIZE(unstake_table,(user)(user_internal)(from)(from_internal)(balance)(unstaked_at))
+	      }; 
             
 
          typedef eosio::multi_index<N(accounts), account> accounts;
          typedef eosio::multi_index<N(stat), currency_stat> stat;
          typedef eosio::multi_index<N(locktbl2), lockup_list> locktbl2;
+      
+         typedef eosio::multi_index<N(pubtbl), pub_table> pubtbl;
+	      typedef eosio::multi_index<N(staketbl), stake_table> staketbl;
+	      typedef eosio::multi_index<N(unstaketbl), unstake_table> unstaketbl;
 
          void sub_balance( account_name owner, asset value );
          void add_balance( account_name owner, asset value, account_name ram_payer );
