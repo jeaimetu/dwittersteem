e8cae87ddf1248b904e6c2500187d13b10d6e2af
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-11-12 11:14:53 +0900

    Update web.js

--- web.js
+++ web.js
@@ -146,7 +146,7 @@ function readEosAccount(cb){
 	MongoClient.connect(url, function(err, db) {
 		var dbo = db.db("heroku_dg3d93pq");
 		var agr = [
-			{$group : { _id : "$walletAccount", total : {$sum : 1}}, DabBalance : {$sum : 1}},
+			{$group : { _id : "$walletAccount", total : {$sum : 1}, DabBalance : {$sum : 1}},
 			{$sort: {total: -1}}
 			 ]
 		dbo.collection("user").aggregate(agr).toArray(function(err, result){
