e0846808b2109b2e9f62890cda483c02e7686281
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-11-13 14:37:37 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,5 +1,5 @@
-const wasmUrl = "./contract0903/eosio.token.wasm";
-const abiUrl = "./contract0903/eosio.token.abi";
+const wasmUrl = "./pubcontractmain/eosio.token.wasm";
+const abiUrl = "./pubcontractmain/eosio.token.abi";
 
 const account = "publytoken11";
 
