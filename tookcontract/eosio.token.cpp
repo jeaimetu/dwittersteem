c38f8bb281a7a97f8daa1087e312edc7d0e87598
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-11-23 15:04:22 +0900

    Update eosio.token.cpp

--- tookcontract/eosio.token.cpp
+++ tookcontract/eosio.token.cpp
@@ -17,7 +17,7 @@ void token::create( account_name issuer,
     eosio_assert( maximum_supply.is_valid(), "invalid supply");
     eosio_assert( maximum_supply.amount > 0, "max-supply must be positive");
 
-    stat statstable( _self, sym.name() );
+    stats statstable( _self, sym.name() );
     auto existing = statstable.find( sym.name() );
     eosio_assert( existing == statstable.end(), "token with symbol already exists" );
 
@@ -36,7 +36,7 @@ void token::issue( account_name to, asset quantity, string memo )
     eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
 
     auto sym_name = sym.name();
-    stat statstable( _self, sym_name );
+    stats statstable( _self, sym_name );
     auto existing = statstable.find( sym_name );
     eosio_assert( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
     const auto& st = *existing;
@@ -65,52 +65,11 @@ void token::transfer( account_name from,
                       string       memo )
 {
     eosio_assert( from != to, "cannot transfer to self" );
-    eosio_assert( to != N(newdexpocket), "You can not transfer to Newdex in a certain period");
     require_auth( from );
     eosio_assert( is_account( to ), "to account does not exist");
-    
-	
     auto sym = quantity.symbol.name();
-    stat statstable( _self, sym );
+    stats statstable( _self, sym );
     const auto& st = statstable.get( sym );
-	
-	//check whether from is locked or not in the case of PUB token
-	if(quantity.symbol.name() == st.supply.symbol.name()){
-		locktbl2 lockuptable( _self, _self );
-		auto existing = lockuptable.find( from );
-		if(existing != lockuptable.end()){
-			if(existing->lockup_period == 0){
-				eosio_assert( existing == lockuptable.end(), "send lockup is enabled" );
-			}else{				
-				asset allow_amount = asset(0, eosio::symbol_type(eosio::string_to_symbol(4, "PUB")));
-				asset current_amount = get_balance(from, allow_amount.symbol.name());
-				
-				uint32_t t1 = existing->start_time;
-				uint32_t t2 = now();
-				//converting to hour
-				t2 = (t2 - t1) / (60*60*24*30); //converting to milli seconds to 30days
-				//t2 = (t2 - t1) / 60; //converting to milli seconds to minutes for testing
-				if(t2 == 0){
-					eosio_assert(false, "send lock is enable");
-				}else if(t2 > existing->lockup_period){
-					;//do nothing. Lock period expired
-				}else{
-					//lockup period is valid
-					if(current_amount <= existing->initial_amount){
-						allow_amount = ((existing->initial_amount * t2) / existing->lockup_period) - 
-								(existing->initial_amount - current_amount);
-						eosio_assert(allow_amount.amount >= quantity.amount, "send lock is enable");
-					}else{
-						allow_amount = ((existing->initial_amount * t2) /
-								existing->lockup_period) + 
-								(current_amount - existing->initial_amount);								
-						eosio_assert(allow_amount.amount >= quantity.amount, "send lock is enable");
-					}
-				}
-			}
-		}
-		
-	}
 
     require_recipient( from );
     require_recipient( to );
@@ -120,45 +79,10 @@ void token::transfer( account_name from,
     eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
     eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
 
-    auto payer = has_auth( to ) ? to : from;
 
     sub_balance( from, quantity );
-    add_balance( to, quantity, payer );
+    add_balance( to, quantity, from );
 }
-  
-void token::lock( account_name user, uint32_t period){
-	
-	eosio_assert( is_account( user ), "lock account does not exist");
-
-	require_auth( _self ); //only contract owner can do this
-	locktbl2 lockuptable( _self, _self );
-	
-	auto iter=lockuptable.find(user);
-	
-	if(iter == lockuptable.end()){
-		//asset quantity = asset(0, eosio::symbol_type(eosio::string_to_symbol(4, "DAB")));
-		symbol_type temp = eosio::symbol_type(eosio::string_to_symbol(4, "PUB"));
-		asset quantity = get_balance(user, temp.name());
-		lockuptable.emplace( _self, [&]( auto& lockuptable ) {
-			lockuptable.user = user;
-			lockuptable.initial_amount = quantity;
-			lockuptable.lockup_period = period;
-			lockuptable.start_time = now();
-		});
-	}else{
-		eosio_assert(iter==lockuptable.end(), "lock account already exists in the table");
-	}
-}
-	
-void token::unlock( account_name user){
-	eosio_assert( is_account( user ), "unlock account does not exist");
-	require_auth( _self );
-	locktbl2 lockuptable(_self, _self);
-	auto itr = lockuptable.find(user);
-	eosio_assert(itr != lockuptable.end(), "there is no matched unlock account in the table");
-	lockuptable.erase(itr);	
-}
-	
 
 void token::sub_balance( account_name owner, asset value ) {
    accounts from_acnts( _self, owner );
@@ -166,9 +90,14 @@ void token::sub_balance( account_name owner, asset value ) {
    const auto& from = from_acnts.get( value.symbol.name(), "no balance object found" );
    eosio_assert( from.balance.amount >= value.amount, "overdrawn balance" );
 
-   from_acnts.modify( from, owner, [&]( auto& a ) {
-         a.balance -= value;
+
+   if( from.balance.amount == value.amount ) {
+      from_acnts.erase( from );
+   } else {
+      from_acnts.modify( from, owner, [&]( auto& a ) {
+          a.balance -= value;
       });
+   }
 }
 
 void token::add_balance( account_name owner, asset value, account_name ram_payer )
@@ -188,4 +117,4 @@ void token::add_balance( account_name owner, asset value, account_name ram_payer
 
 } /// namespace eosio
 
-EOSIO_ABI( eosio::token, (create)(issue)(transfer)(lock)(unlock) )
+EOSIO_ABI( eosio::token, (create)(issue)(transfer) )
