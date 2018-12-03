f4933d26e539d9bf0a8950a24aed566661f3b423
teddy <jeaiemtu@naver.com>
2018-12-03 18:42:36 +0000

    adding free transfer

---
+++ tookcontracttemp/eosio.token.cpp
@@ -0,0 +1,149 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+
+#include "eosio.token.hpp"
+
+namespace eosio {
+
+void token::create( account_name issuer,
+                    asset        maximum_supply )
+{
+    require_auth( _self );
+
+    auto sym = maximum_supply.symbol;
+    eosio_assert( sym.is_valid(), "invalid symbol name" );
+    eosio_assert( maximum_supply.is_valid(), "invalid supply");
+    eosio_assert( maximum_supply.amount > 0, "max-supply must be positive");
+
+    stat statstable( _self, sym.name() );
+    auto existing = statstable.find( sym.name() );
+    eosio_assert( existing == statstable.end(), "token with symbol already exists" );
+
+    statstable.emplace( _self, [&]( auto& s ) {
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer;
+    });
+}
+
+
+void token::issue( account_name to, asset quantity, string memo )
+{
+    auto sym = quantity.symbol;
+    eosio_assert( sym.is_valid(), "invalid symbol name" );
+    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    auto sym_name = sym.name();
+    stat statstable( _self, sym_name );
+    auto existing = statstable.find( sym_name );
+    eosio_assert( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
+    const auto& st = *existing;
+
+    require_auth( st.issuer );
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must issue positive quantity" );
+
+    eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    eosio_assert( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+
+    statstable.modify( st, 0, [&]( auto& s ) {
+       s.supply += quantity;
+    });
+
+    add_balance( st.issuer, quantity, st.issuer );
+
+    if( to != st.issuer ) {
+       SEND_INLINE_ACTION( *this, transfer, {st.issuer,N(active)}, {st.issuer, to, quantity, memo} );
+    }
+}
+
+void token::transfer( account_name from,
+                      account_name to,
+                      asset        quantity,
+                      string       memo )
+{
+    eosio_assert( from != to, "cannot transfer to self" );
+  
+	//Prevent non-negotiated listing (S) 2018.11.27
+	
+	//Newdex Case
+    	eosio_assert( to != N(newdexpocket), "You can not transfer to Newdex in a certain period");	
+
+	//WhaleEX
+	eosio_assert( to != N(whaleextrust), "You can not transfer to this exchange in a certain period");
+	eosio_assert( to != N(heydcmjrhege), "You can not transfer to this exchange in a certain period");
+	
+	eosio_assert( from != N(whaleextrust), "You can not transfer to this exchange in a certain period");
+	eosio_assert( from != N(heydcmjrhege), "You can not transfer to this exchange in a certain period");
+	//Btex
+	eosio_assert( to != N(eosbtexbonus), "You can not transfer to this exchange in a certain period");
+	eosio_assert( to != N(eosconvertbt), "You can not transfer to this exchange in a certain period");
+	eosio_assert( to != N(eosbtexteams), "You can not transfer to this exchange in a certain period");
+	eosio_assert( to != N(btexexchange), "You can not transfer to this exchange in a certain period");
+	eosio_assert( to != N(eosbtextoken), "You can not transfer to this exchange in a certain period");
+	eosio_assert( to != N(eosbtexfunds), "You can not transfer to this exchange in a certain period");
+	
+	eosio_assert( from != N(eosbtexbonus), "You can not transfer to this exchange in a certain period");
+	eosio_assert( from != N(eosconvertbt), "You can not transfer to this exchange in a certain period");
+	eosio_assert( from != N(eosbtexteams), "You can not transfer to this exchange in a certain period");
+	eosio_assert( from != N(btexexchange), "You can not transfer to this exchange in a certain period");
+	eosio_assert( from != N(eosbtextoken), "You can not transfer to this exchange in a certain period");
+	eosio_assert( from != N(eosbtexfunds), "You can not transfer to this exchange in a certain period");
+	
+	//Prevent non-negotiated listing ()
+
+    require_auth( from );
+    eosio_assert( is_account( to ), "to account does not exist");
+    auto sym = quantity.symbol.name();
+    stat statstable( _self, sym );
+    const auto& st = statstable.get( sym );
+
+    require_recipient( from );
+    require_recipient( to );
+
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must transfer positive quantity" );
+    eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+
+    sub_balance( from, quantity );
+    add_balance( to, quantity, from );
+}
+
+void token::sub_balance( account_name owner, asset value ) {
+   accounts from_acnts( _self, owner );
+
+   const auto& from = from_acnts.get( value.symbol.name(), "no balance object found" );
+   eosio_assert( from.balance.amount >= value.amount, "overdrawn balance" );
+
+
+   if( from.balance.amount == value.amount ) {
+      from_acnts.erase( from );
+   } else {
+      from_acnts.modify( from, owner, [&]( auto& a ) {
+          a.balance -= value;
+      });
+   }
+}
+
+void token::add_balance( account_name owner, asset value, account_name ram_payer )
+{
+   accounts to_acnts( _self, owner );
+   auto to = to_acnts.find( value.symbol.name() );
+   if( to == to_acnts.end() ) {
+      to_acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = value;
+      });
+   } else {
+      to_acnts.modify( to, 0, [&]( auto& a ) {
+        a.balance += value;
+      });
+   }
+}
+
+} /// namespace eosio
+
+EOSIO_ABI( eosio::token, (create)(issue)(transfer) )
