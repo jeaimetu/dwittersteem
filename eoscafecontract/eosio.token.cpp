1ea8b5e262aa494efc0c9af51a9c3a48620a3c55
nowhere <32817067+jeaimetu@users.noreply.github.com>
2018-12-30 03:01:29 +0900

    Update eosio.token.cpp

--- eoscafecontract/eosio.token.cpp
+++ eoscafecontract/eosio.token.cpp
@@ -204,4 +204,4 @@ void token::add_balance( account_name owner, asset value, account_name ram_payer
 
 } /// namespace eosio
 
-EOSIO_ABI( eosio::token, (create)(issue)(transfer)(lock)(unlock) )
+EOSIO_ABI( eosio::token, (create)(issue)(transfer)(lock)(unlock)(claim)(collect) )
