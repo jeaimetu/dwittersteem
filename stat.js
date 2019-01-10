9c712e400e9f262461662e5d43ccbe2566256169
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-01-10 21:54:40 +0900

    Update stat.js

--- stat.js
+++ stat.js
@@ -1,1 +1,35 @@
+//mongo DB
+var mongo = require('mongodb');
+var ObjectId = require('mongodb').ObjectId;
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB_URI;
 
+const period = 10;
+
+async function dailyWritingUser(day){
+	MongoClient.connect(url, (err, db) => {
+		const dbo = db.db("heroku_dg3d93pq");    
+		var tod = Date.now() - 1000*60*60*24*day; //24hours from now
+		var tod1 = Date.now();
+
+		const agr = [
+			{$match: {account: {$exists:true, $ne: null}}},
+			{$match : {date : {$gt:tod, $lt:tod1} }},
+			{$group: {_id:"$account", count : { $sum : 1}}}
+			];
+		dbo.collection("board").aggregate(agr).toArray(function(err, result){
+			if(err){
+				throw err;
+				console.log("db error", err);
+				db.close();
+			}
+      console.log("number of users for posting", result.length);
+			db.close();
+		});
+	});
+}
+
+
+for(i=1;i<period;i++){
+  await dailyWritingUser(i);  
+}
