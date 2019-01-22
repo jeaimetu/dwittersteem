748184098bc95e091c108183f2084f2c727322bb
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-01-22 16:01:52 +0900

    Update random.js

--- random.js
+++ random.js
@@ -2,5 +2,5 @@ const s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345.";
 
 
 for(i = 0;i<100;i++){
-  console.log("random id", Math.random().toString(s).substr(2, 12);
+  console.log("random id", Math.random().toString(s).substr(2, 12));
 }
