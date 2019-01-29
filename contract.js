534a8e75e088198426cf6eb3fcb9df83ed18bb0d
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-01-29 21:50:38 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,7 +1,7 @@
-const wasmUrl = "./eoscafecontract/eosio.token.wasm";
-const abiUrl = "./eoscafecontract/eosio.token.abi";
+const wasmUrl = "./tookcontract/eosio.token.wasm";
+const abiUrl = "./tookcontract/eosio.token.abi";
 
-const account = "thebeantoken";
+const account = "taketooktook";
 
 Eos = require('eosjs');
 const fs = require('fs');
@@ -16,11 +16,11 @@ var url = process.env.MONGODB_URI;
 
 
 config = {
-  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
-  //chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",// 32 byte (64 char) hex string
+  //chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+  chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473",// 32 byte (64 char) hex string
   keyProvider: process.env.key, // WIF string or array of keys..
-  httpEndpoint: 'https://mainnet.eoscalgary.io',
-  //httpEndpoint:	"http://193.93.219.219:8888",
+  //httpEndpoint: 'https://mainnet.eoscalgary.io',
+  httpEndpoint:	"https://jungle2.cryptolions.io:443",
   expireInSeconds: 60,
   broadcast: true,
   verbose: false, // API activity
@@ -97,8 +97,8 @@ abi = fs.readFileSync(abiUrl);
 
 //console.log("Wasm", wasm);
 //console.log("Abi", abi);
-eos.setcode("thebeantoken", 0, 0, wasm) // @returns {Promise}
-eos.setabi("thebeantoken", JSON.parse(abi)) // @returns {Promise}
+eos.setcode("taketooktook", 0, 0, wasm) // @returns {Promise}
+eos.setabi("taketooktook", JSON.parse(abi)) // @returns {Promise}
 
 
 
