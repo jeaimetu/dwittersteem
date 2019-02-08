8b6dc9b3f46e0747c656b1c97d19ee4e742ae88d
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-02-08 09:21:26 +0900

    Update eosio.token.cpp

--- tookcontract/eosio.token.cpp
+++ tookcontract/eosio.token.cpp
@@ -166,7 +166,7 @@ void token::unlock( account_name user){
 void token::newaccount(account_name iuser){
 	require_auth( _self );
 	
-	tooktbl tooktable(_self, iuser);
+	tooktbl2 tooktable(_self, iuser);
 	auto iter = tooktable.find(iuser);
 	
 	eosio_assert(iter == tooktable.end(), "account already exist");
@@ -196,7 +196,7 @@ void token::refund(account_name from, account_name to){
 void token::updatetp(account_name user, asset quantity){
 	require_auth( _self );
 	
-	tooktbl tooktable(_self, user);
+	tooktbl2 tooktable(_self, user);
 	auto iter = tooktable.find(user);
 	eosio_assert(iter != tooktable.end(), "account does not exist");
 	
