ecba94c8fb66b3c49d99a5a3a861b74bfa4a353f
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-02-26 15:12:49 +0900

    Update web.js

--- web.js
+++ web.js
@@ -49,6 +49,80 @@ app.use(require('express-session')({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+function readData(account, page, cb){
+	console.log("in reaData", account, page);
+	const pageSize = 20;
+	MongoClient.connect(url, function(err, db) {
+   		var dbo = db.db("heroku_dg3d93pq");
+		var tod = Date.now();
+		//ToDo : add image path to response
+   		//dbo.collection("board").find({}).sort({date: -1}).toArray(function(err, result){
+		
+		var agr = [	
+			
+
+			  {$sort: {"date" : -1}},
+			{$skip : (pageSize * page) - pageSize},
+			   {$limit : pageSize},
+			{$lookup:
+			    { from: 'user',
+			   localField: 'account',
+			   foreignField : 'account',
+			   as : 'userdetails'
+			    }
+			   }
+			];
+		
+		dbo.collection("board").aggregate(agr).toArray(function(err, result){
+			    			if (err) throw err;
+			var body = []; // empty array
+			var picUrl;
+			console.log("Readdata size", result.length);
+			//console.log("aggregate data", result);
+			//onsole.log("before for loop");
+			
+			for(i = 0; i < result.length ; i++){				
+				//console.log("in for loop");
+				if(result[i].userdetails[0] == undefined){
+					picUrl = "0.png";
+					//console.log("undefined case");
+				}
+				else{
+					//console.log("define case");
+					var picUrl = result[i].userdetails[0].profile;
+				}
+				
+
+				const votingenable = "true";
+				//check parent id
+				var parentId = null;
+				if(result[i].hasOwnProperty("parentid"))
+					parentId = result[i].parentid
+					
+				
+				body.push({id: result[i]._id, account: result[i].account, data : result[i].data, date : result[i].date,
+					  voting : result[i].voting,  profile : picUrl, votingenable : votingenable, parentid : parentId });
+				
+			}
+			//generating votingenable flag
+			var findQuery = { account : account };
+			dbo.collection("voting").find(findQuery).toArray(function(err, votingTable){
+				if(err) throw err;
+				console.log("voting table query result",votingTable.length, account, body.length);
+				for(i = 0;i < body.length;i++){
+					for(j = 0;j<votingTable.length;j++)
+						if(body[i].id == votingTable[j].boardId){
+							body[i].votingenable = "false";
+							break;
+						}
+				}
+				cb(body);
+    				db.close();   
+			});
+   		});
+  	}); 
+}
+
 function fnIsLogin(req){
 	  if(req.body.id != undefined && req.body.id != null ){
 		  var id = req.body.id;
