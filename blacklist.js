f4616220f35256ccadfa7312912ac60b8f7f1dc1
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-02-27 12:59:51 +0900

    Create blacklist.js

---
+++ blacklist.js
@@ -0,0 +1,19 @@
+const abuser = ['tommy',
+                'sakina',
+                'jony',
+                'sakina',
+                'xhono',
+                'king',
+                'kai',
+                'ching',
+                'ansari',
+                'kigsatan',
+                '1234',
+                'leegw3636'];
+
+
+const wallet = ['arslanstare1'];
+
+
+module.exports.abuser = abuser;
+module.exports.wallet = wallet;
