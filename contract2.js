ab8392649eaeeee11c472810abe4ff55429f4163
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-11-12 18:22:10 +0900

    Update contract2.js

--- contract2.js
+++ contract2.js
@@ -9,7 +9,7 @@ const Eos = require('eosjs');
 config = {
   chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", // 32 byte (64 char) hex string
   keyProvider: process.env.key, // WIF string or array of keys..
-  httpEndpoint: 'https://proxy.eosnode.tools',
+  httpEndpoint: 'https://user-api.eoseoul.io:443',
   expireInSeconds: 60,
   broadcast: true,
   verbose: false, // API activity
