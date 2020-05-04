72b547b1cedd8f6fc02bfa7fac0d3cbc3eb175d6
nowhere <32817067+jeaimetu@users.noreply.github.com>
2020-05-04 16:00:40 +0900

    Update eosio.token.hpp

--- dabblecontract/eosio.token.hpp
+++ dabblecontract/eosio.token.hpp
@@ -4,8 +4,9 @@
  */
 #pragma once
 
-#include <eosiolib/asset.hpp>
-#include <eosiolib/eosio.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/eosio.hpp>
+#include <eosio/system.hpp>
 
 #include <string>
 
@@ -17,15 +18,15 @@ namespace eosio {
 
    using std::string;
 
-   class token : public contract {
+   class [[eosio::contract("eosio.token")]] token : public contract {
       public:
-         token( account_name self ):contract(self){}
-
+         [[eosio::action]]  
          void create( account_name issuer,
                       asset        maximum_supply);
-
+	 [[eosio::action]]  
          void issue( account_name to, asset quantity, string memo );
-
+	 
+	 [[eosio::action]]  
          void transfer( account_name from,
                         account_name to,
                         asset        quantity,
@@ -33,65 +34,65 @@ namespace eosio {
          void postlimit( account_name from,
                          string time,
                          string memo);
-	 //@abi action
+	 [[eosio::action]]  
 	 void post(string author, string content, string link);
-	 //@abi action
+	 [[eosio::action]]  
 	 void vote(string from, string to, string link);
-	 //@abi action
+	 [[eosio::action]]  
 	 void reply(string author, string to, string content, string link);
-	 //@abi action
+	 [[eosio::action]]  
 	 void payout(string to, asset quantity, string remarks);
       
-         inline asset get_supply( symbol_name sym )const;
+         inline asset get_supply( symbol_code sym )const;
          
-         inline asset get_balance( account_name owner, symbol_name sym )const;
+         inline asset get_balance( account_name owner, symbol_code sym )const;
 
       private:
-// @abi table accounts i64
-         struct account {
+
+         struct [[eosio::table]] account {
             asset    balance;
 
-            uint64_t primary_key()const { return balance.symbol.name(); }
+            uint64_t primary_key()const { return balance.symbol.code().raw(); }
          };
-// @abi table stat i64
-         struct currency_stat {
+
+	   struct [[eosio::table]] currency_stat {
             asset          supply;
             asset          max_supply;
-            account_name   issuer;
+            name   issuer;
 
-            uint64_t primary_key()const { return supply.symbol.name(); }
+            uint64_t primary_key()const { return supply.symbol.code().raw(); }
          };
       
            
-         typedef eosio::multi_index<N(accounts), account> accounts;
-         typedef eosio::multi_index<N(stat), currency_stat> stat;
+         typedef eosio::multi_index<"accounts"_n, account> accounts;
+         typedef eosio::multi_index<"stat"_n, currency_stat> stat;
       
       
 
 
-         void sub_balance( account_name owner, asset value );
-         void add_balance( account_name owner, asset value, account_name ram_payer );
+         void sub_balance( name owner, asset value );
+         void add_balance( name owner, asset value, name ram_payer );
 
       public:
          struct transfer_args {
-            account_name  from;
-            account_name  to;
+            name  from;
+            name  to;
             asset         quantity;
             string        memo;
          };
    };
 
-   asset token::get_supply( symbol_name sym )const
+   asset token::get_supply( symbol_code sym )const
    {
-      stat statstable( _self, sym );
-      const auto& st = statstable.get( sym );
+      stat statstable( get_self(), sym.raw() );
+      const auto& st = statstable.get( sym.raw() );
       return st.supply;
    }
 
-   asset token::get_balance( account_name owner, symbol_name sym )const
+   asset token::get_balance( name owner, symbol_code  sym )const
    {
-      accounts accountstable( _self, owner );
-      const auto& ac = accountstable.get( sym );
+      accounts accountstable( get_self(), owner.value );
+      const auto& ac = accountstable.get( sym.raw()  );
       return ac.balance;
    }
 
