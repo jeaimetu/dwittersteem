b6888566b8fe42f9b2d97e821200bb5b1661fb39
nowhere <32817067+jeaimetu@users.noreply.github.com>
2020-05-04 17:58:05 +0900

    Update eosio.token.cpp

--- dabblecontract/eosio.token.cpp
+++ dabblecontract/eosio.token.cpp
@@ -29,7 +29,7 @@ void token::create( const name&  issuer,
 }
 
 
-void token::issue(const  name&  to, const asset& quantity,  string memo )
+void token::issue(const  name&  to, const asset& quantity,  const string& memo )
 {
     auto sym = quantity.symbol;
     check( sym.is_valid(), "invalid symbol name" );
