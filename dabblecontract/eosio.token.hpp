c9eea7b32e72e03c1257b04586ca0a5ff8622e2b
nowhere <32817067+jeaimetu@users.noreply.github.com>
2020-05-04 16:34:23 +0900

    Update eosio.token.hpp

--- dabblecontract/eosio.token.hpp
+++ dabblecontract/eosio.token.hpp
@@ -19,6 +19,8 @@ namespace eosio {
    using std::string;
 
    class [[eosio::contract("eosio.token")]] token : public contract {
+	   using contract::contract;
+	   
       public:
          [[eosio::action]]  
          void create( name  issuer,
