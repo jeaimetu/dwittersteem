351f6d901cb38db89ac3f5559df149656b4b2e89
Teddy Kim <jeaimetu@naver.com>
2021-02-03 02:45:18 +0900

    adding new eosjs

---
+++ contractnew.js
@@ -0,0 +1,190 @@
+const wasmUrl = "./tookcontract/eosio.token.wasm";
+const abiUrl = "./tookcontract/eosio.token.abi";
+
+const account = "taketooktook";
+
+Eos = require('eosjs');
+const fs = require('fs');
+
+//mongo DB
+var mongo = require('mongodb');
+var ObjectId = require('mongodb').ObjectId;
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB_URI;
+
+
+
+//mainnet
+config = {
+  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",//mainnet
+  //chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473",//testnet
+  //chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473",// 32 byte (64 char) hex string
+  keyProvider: process.env.key, // WIF string or array of keys..
+  httpEndpoint: 'https://user-api.eoseoul.io:443',
+  //httpEndpoint:	"https://jungle2.cryptolions.io:443",
+  expireInSeconds: 60,
+  broadcast: true,
+  verbose: false, // API activity
+  sign: true
+}
+
+
+eos = Eos(config);
+
+/*
+function transfer(from, to, amount, msg){
+
+
+	eos.transaction("eoscafekorea").then(myaccount => {
+		myaccount.transfer(from,to, amount + " " + "DAB", msg);
+	});
+
+
+
+
+}
+*/
+
+function ttt(account,callback){
+	MongoClient.connect(url, function(err, db) {
+		const dbo = db.db("heroku_dg3d93pq");
+		const findQuery = {account : "길막테디"};
+		dbo.collection("user").findOne(findQuery, function(err, resFind){
+			 if(err) throw err;
+			 const sendAmount = resFind.wallet;
+			
+transfer("eoscafekorea","gyydoojzgige","0.0001", "test").then((output)=>{
+	console.log("OK");
+				 const updateQuery = {account : account};
+				 const myObj = {$set : {wallet : "0"}};
+				 dbo.collection("user").updateOne(updateQuery, myObj,function(err, resFind){
+					 if(err) throw err;
+					 db.close();
+					 callback("success");
+				 }); //end of updateOne
+
+}).catch((err)=>{
+	console.log("transfer error");
+	db.close();
+	callback("success");
+});
+		});
+	});
+	
+}
+
+
+//ttt("길막테디", (result) => {console.log(result)});
+
+
+
+
+
+/*
+eos.transaction(tr => {
+	  tr.buyrambytes({
+    payer: 'lghackerthon',
+    receiver: 'lghackerthon',
+    bytes: 400*1024
+  })
+});
+*/
+
+  
+
+wasm = fs.readFileSync(wasmUrl);  
+abi = fs.readFileSync(abiUrl);
+
+//console.log("Wasm", wasm);
+//console.log("Abi", abi);
+eos.setcode("taketooktook", 0, 0, wasm) // @returns {Promise}
+eos.setabi("taketooktook", JSON.parse(abi)) // @returns {Promise}
+
+
+async function getTransaction(){
+	const transaction = await eos.getTransaction("94edddbaaa0a98872f6300768e6321effe5ccc91a3834eddfad9d59ded9db2d7")
+	//console.log(transaction);
+	//console.log(transaction.trx);
+	console.log(transaction.trx.trx.actions[0].data.quantity);
+}
+
+//setTimeout(getTransaction, 3000);
+//getTransaction();
+
+async function createToken(account){
+  await eos.transaction(account, myaccount => {
+
+  // Create the initial token with its max supply
+  // const options = {authorization: 'myaccount'} // default
+    //500,000,000 is the base. Annual inflation is 5% to 5 years. Others will be burned.
+  const options = { authorization: [ `taketooktook@active` ] };
+  myaccount.create(account, '10000000000.0000 TOOK', options)//, options)
+
+  // Issue some of the max supply for circulation into an arbitrary account
+  myaccount.issue(account, '10000000000.0000 TOOK', 'issue', options)
+})
+
+  //const balance = await eos.getCurrencyBalance(account, account, 'DAB')
+  //console.log('Currency Balance', balance)
+}
+
+
+async function transfer(from, to, amount){
+	const myaccount = await eos.contract(from);
+	await myaccount.transfer(from, to, amount + " " + "BEANS","");
+}
+
+
+
+//createToken("taketooktook");
+
+if(process.env.dist != "true")
+	return;
+
+eos.getCurrencyBalance("thebeantoken", "thebeantoken", 'BEAN').then(function(result){
+	console.log("thebeantoken balance", result);
+});
+
+eos.getCurrencyBalance("thebeantoken", "thebeantoken", 'BEAN').then(function(result){
+	console.log("thebeantoken balance", result);
+});
+
+//transfer("eoscafekorea","awesometeddy","1000.0000");
+               
+
+
+
+//transfer("eoscafekorea","gu2dknbqgage",1000.0000);
+/*
+eos.transaction(eos =>
+  {
+    eos.issue('awesometeddy', '1111.0000 DAB', "initial token transfer");
+});
+*/
+//eos.getCurrencyBalance("eoscafekorea", "eoscafekorea", 'DAB').then(function(result){
+//	console.log("eoscafekorea balance", result);
+//});
+/*
+options = {
+  authorization: 'eoscafekorea@active',
+  broadcast: true,
+  sign: true
+}
+eos.transfer('eoscafekorea', 'awesometeddy', '1000.0000 DAB', '',options)
+*/
+
+//eos.transaction("eoscafekorea", myaccount => {
+// Issue some of the max supply for circulation into an arbitrary account
+	//myaccount.issue("eoscafekorea", '20000000.0000 DAB', 'issue')
+  //myaccount.transfer("eoscafekorea", "g44dqnbsg4ge", '9991000.0000 DAB', "DAB Adviser Fund");
+	  //myaccount.transfer("eoscafekorea", "abraininajar", '10000000.0000 DAB', "DAB Founder Fund");
+	  //myaccount.transfer("eoscafekorea", "gu2dmojqgqge", '10000000.0000 DAB', "DAB Founder Fund");
+	  //myaccount.transfer("eoscafekorea", "gu2dknbqgage", '10000000.0000 DAB', "DAB Founder Fund");
+	  //myaccount.transfer("eoscafekorea", "gu3dinjtgage", '10000000.0000 DAB', "DAB Founder Fund");
+	  //myaccount.transfer("eoscafekorea", "q1q1q1q1q1q1", '10000000.0000 DAB', "DAB Founder Fund");
+//});
+
+
+                  
+                                                                                                                     
+                                                                                                                     
